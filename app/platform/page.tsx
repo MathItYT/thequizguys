6dd439a4050fe7b4c6a0ffa439ce6e9f312c4b93
MathItYT <benjamin.ubilla@uc.cl>
2025-02-02 15:06:59 -0300

    fix 'see server logs' message and check if it's part of the discord server

--- app/platform/page.tsx
+++ app/platform/page.tsx
@@ -13,18 +13,31 @@ export default async function Page({
     if (!code) {
         redirect("/login");
     }
-    const { user, roles } = await getDiscordData(code);
-    const { username } = user;
-    return (
-        <div>
-            <h1>¡Hola, {username}!</h1>
-            <p>¡Bienvenido a la plataforma!</p>
-            {roles.isMathHelper && <p>¡Eres un ayudante de matemáticas!</p>}
-            {roles.isPhysicsHelper && <p>¡Eres un ayudante de física!</p>}
-            {roles.isChemistryHelper && <p>¡Eres un ayudante de química!</p>}
-            {roles.isBiologyHelper && <p>¡Eres un ayudante de biología!</p>}
-            {roles.isComputerScienceHelper && <p>¡Eres un ayudante de ciencias de la computación!</p>}
-            {roles.isMathLikeUserId && <p>¡Eres MathLike!</p>}
-        </div>
-    );
+    try {
+        const { user, roles } = await getDiscordData(code);
+        const username = user.username;
+        if (!roles.isDiscordGuildMember) {
+            return (
+                <div>
+                    <h1>¡Hola, {username}!</h1>
+                    <p>¡Debes ser miembro de nuestro servidor de Discord para poder acceder a la plataforma!</p>
+                </div>
+            );
+        }
+        return (
+            <div>
+                <h1>¡Hola, {username}!</h1>
+                <p>¡Bienvenido a la plataforma!</p>
+                {roles.isMathHelper && <p>¡Eres un ayudante de matemáticas!</p>}
+                {roles.isPhysicsHelper && <p>¡Eres un ayudante de física!</p>}
+                {roles.isChemistryHelper && <p>¡Eres un ayudante de química!</p>}
+                {roles.isBiologyHelper && <p>¡Eres un ayudante de biología!</p>}
+                {roles.isComputerScienceHelper && <p>¡Eres un ayudante de ciencias de la computación!</p>}
+                {roles.isMathLikeUserId && <p>¡Eres MathLike!</p>}
+            </div>
+        );
+    } catch (error) {
+        console.error(error);
+        redirect("/login");
+    }
 }
